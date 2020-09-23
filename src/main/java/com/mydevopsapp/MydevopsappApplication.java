cd56e9dadfac4abf9f4d6dd69cf072ee6d2e7765
Mayur Chinchalkar <mayurchinchalkar16@gmail.com>
2020-09-23 23:27:51 +0530

    Commiting the git file

---
+++ src/main/java/com/mydevopsapp/MydevopsappApplication.java
@@ -0,0 +1,13 @@
+package com.mydevopsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydevopsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydevopsappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mydevopsapp/MydevopsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydevopsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydevopsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
